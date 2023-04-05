75511c348d5e0396e3753cfd0c4ff7240946d6a4
Camila13L <scamilalauram@outlook.es>
2023-04-05 19:53:54 -0400

    add hadler for validations

---
+++ backend-service/src/Core/CRUD.Challenge.Core.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿namespace CRUD.Challenge.Core.Application.Exceptions;
+
+using System;
+using FluentValidation.Results;
+
+public class ValidationException : Exception
+{
+
+    public List<string> Errors { get; }
+
+    public ValidationException() : base("One or more validation errors found")
+    {
+        Errors = new List<string>();
+    }
+
+    public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+    {
+        foreach (var failure in failures)
+        {
+            this.Errors.Add(failure.ErrorMessage);
+        }
+    }
+}
+
