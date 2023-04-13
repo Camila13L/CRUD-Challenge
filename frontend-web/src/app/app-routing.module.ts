b8289bade1fa942ee31ff19a255bd93f3eef6350
Sara Camila Laura Manzaneda <SCamilaLauraM@outlook.es>
2023-04-12 21:26:54 -0400

    add login styles and some actions

--- frontend-web/src/app/app-routing.module.ts
+++ frontend-web/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),ReactiveFormsModule,],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ frontend-web/src/app/modules/auth/components/background/background.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+})
+export class BackgroundComponent {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
